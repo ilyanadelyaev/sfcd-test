36ba0700903dc9927ba2f0f607aaf236e3effbc3
ilyanadelyaev <nadelyaev.ilya@gmail.com>
2016-01-27 14:39:31 +0300

    fix password hashing

--- sfcd/misc.py
+++ sfcd/misc.py
@@ -6,5 +6,5 @@ def hash_password(password):
     salt = uuid.uuid4().hex
     return (hashlib.sha512(password + salt).hexdigest(), salt)
 
-def validate_password_hash(password, salt, password_hash):
-    return hashlib.sha512(password + salt).hexdigest() == password_hash
+def validate_password_hash(password, hashed, salt):
+    return hashlib.sha512(password + salt).hexdigest() == hashed
--- sfcd/test/test_misc.py
+++ sfcd/test/test_misc.py
@@ -4,5 +4,5 @@ import sfcd.misc
 class TestMisc:
     def test__hash_password(self, password):
         hashed, salt = sfcd.misc.hash_password(password)
-        assert sfcd.misc.validate_password_hash(password, salt, hashed)
+        assert sfcd.misc.validate_password_hash(password, hashed, salt)
 
