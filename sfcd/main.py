e67d48ea6c6bb37dfc2e056ee2ce7c37798bcee3
ilyanadelyaev <nadelyaev.ilya@gmail.com>
2016-01-27 18:10:40 +0300

    add main module to run app

---
+++ sfcd/main.py
@@ -0,0 +1,18 @@
+import sfcd
+import sfcd.config
+
+
+if __name__ == '__main__':
+    # get db params from config
+    db_type = sfcd.config.DB_TYPE
+    db_url = None
+    if sfcd.config.DB_TYPE == 'sql':
+        db_url = sfcd.config.SQL_DB_URL
+    elif sfcd.config.DB_TYPE == 'mongo':
+        db_url = sfcd.config.MONGO_DB_URL
+
+    # create app
+    sfcd.app = sfcd.Application(db_type, db_url)
+
+    # run web app
+    sfcd.app.web_view.run(debug=sfcd.config.FLASK_DEBUG)
